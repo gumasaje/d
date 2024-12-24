e1090e3e7697317ac56f034c6e4948fb38da9227
gumasaje <geniarti@naver.com>
2024-12-24 13:53:01 +0900

    [Bronze V] Title: 윤년, Time: 176 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        sc.close();
+        
+        if(year%4==0){
+            if(year%100!=0)
+                System.out.print("1");
+            else if(year%400==0)
+                System.out.print("1");
+            else
+                System.out.print("0");
+        }
+        else
+            System.out.print("0");
+    }
+}
