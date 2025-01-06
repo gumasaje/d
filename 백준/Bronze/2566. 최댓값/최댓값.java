e41b788a37a8fb5df9184a3557f67770737e6f42
gumasaje <geniarti@naver.com>
2025-01-06 15:34:57 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 18532 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int arr[][] = new int[9][9];
+        
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++)
+                arr[i][j] = sc.nextInt();
+        }
+        sc.close();
+        
+        int max = 0;
+        int n = 1;
+        int m = 1;
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(arr[i][j] > max){
+                    max = arr[i][j];
+                    n = i+1;
+                    m = j+1;
+                }
+            }
+        }
+        
+        System.out.println(max);
+        System.out.print(n+" "+m);
+        
+    }
+}
