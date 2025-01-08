3013f4e914f9e2069510b568c8125f0c2830fa9f
gumasaje <geniarti@naver.com>
2025-01-08 16:37:05 +0900

    [Bronze III] Title: 오븐 시계, Time: 192 ms, Memory: 18428 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
-
 public class Main{
     public static void main(String[] args){
+        
         Scanner sc = new Scanner(System.in);
         int H = sc.nextInt();
         int M = sc.nextInt();
@@ -9,13 +9,14 @@ public class Main{
         sc.close();
         
         if(M+T>=60){
-            H = H+((M+T)/60);
+            H += (M+T)/60;
             M = (M+T)%60;
             if(H>=24)
-                H = H-24;
+                H -= 24;
         }
         else
             M = M+T;
+        
         System.out.print(H+" "+M);
     }
 }
