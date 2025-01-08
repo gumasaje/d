ad808f1e7f1db5724c7d4f5379a485e97aa1324e
gumasaje <geniarti@naver.com>
2025-01-08 16:21:20 +0900

    [Bronze V] Title: 사분면 고르기, Time: 168 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
+        
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int y = sc.nextInt();
@@ -8,15 +9,15 @@ public class Main{
         
         if(x>0){
             if(y>0)
-                System.out.print("1");
+                System.out.print('1');
             else
-                System.out.print("4");
+                System.out.print('4');
         }
         else{
             if(y>0)
-                System.out.print("2");
+                System.out.print('2');
             else
-                System.out.print("3");
-        }            
+                System.out.print('3');
+        }
     }
 }
