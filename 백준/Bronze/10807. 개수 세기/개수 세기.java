242cf863eb47648487cc436baf31b06d0dd596e1
gumasaje <geniarti@naver.com>
2025-01-09 15:30:16 +0900

    [Bronze V] Title: 개수 세기, Time: 176 ms, Memory: 17796 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         
-        int count = 0;
-        
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int N[] = new int[n];
+        int N = sc.nextInt();
+        int arr[] = new int[N];
         
-        for(int i=0; i<N.length; i++)
-            N[i] = sc.nextInt();
+        for(int i=0; i<arr.length; i++)
+            arr[i] = sc.nextInt();
         
         int v = sc.nextInt();
         sc.close();
         
-        for(int j=0; j<N.length; j++){
-            if(N[j]==v)
-                count++;
+        int count = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == v)
+                count += 1;
         }
+        
         System.out.print(count);
     }
 }
