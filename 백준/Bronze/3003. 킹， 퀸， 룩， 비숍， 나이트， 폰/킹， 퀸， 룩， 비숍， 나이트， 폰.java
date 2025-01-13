b0548dbae5e56baba3ce8395278a807a5290b8db
gumasaje <geniarti@naver.com>
2025-01-14 03:23:53 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 188 ms, Memory: 18364 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int arr1[] = {1, 1, 2, 2, 2, 8};
+        
+        int arr1[] = {1,1,2,2,2,8};
         int arr2[] = new int[6];
         
-        for(int i=0; i<arr1.length; i++)
-            arr2[i] = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<arr2.length; i++) {
+        	arr2[i] = sc.nextInt();
+        	arr1[i] -= arr2[i];
+        }
         sc.close();
         
-        for(int i=0; i<arr1.length; i++){
-            arr2[i] = arr1[i] - arr2[i];
-            System.out.print(arr2[i]+" ");
-        }
+        for(int i=0; i<arr1.length; i++)
+        	System.out.print(arr1[i]+" ");   
     }
 }
