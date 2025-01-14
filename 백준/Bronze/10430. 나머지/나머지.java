92312428612eac0c0275a7ff92dafd2d8e1fb89b
gumasaje <geniarti@naver.com>
2025-01-15 08:45:29 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,20 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
-        int A = in.nextInt();
-        int B = in.nextInt();
-        int C = in.nextInt();
-        
-        in.close();
-        
-        System.out.println((A+B)%C);
-        System.out.println(((A%C)+(B%C))%C);
-        System.out.println((A*B)%C);
-        System.out.println(((A%C)*(B%C))%C);
-        
-       
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		System.out.println((A+B)%C);
+		System.out.println(((A%C)+(B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println((A%C)*(B%C)%C);
+		
+	}
 }
