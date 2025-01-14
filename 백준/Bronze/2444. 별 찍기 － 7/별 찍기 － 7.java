98734285fa96df34eb13808bbbe8e7a7a6628417
gumasaje <geniarti@naver.com>
2025-01-15 04:37:18 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 420 ms, Memory: 23184 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -10,7 +10,7 @@ public class Main{
                 System.out.print("*");
             System.out.println();
         }
-        for(int i=N-1; i>=0; i--){
+        for(int i=N-1; i>=1; i--){
             for(int j=0; j<N-i; j++)
                 System.out.print(" ");
             for(int j=0; j<2*i-1; j++)
