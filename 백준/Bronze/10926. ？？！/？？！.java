8150f8dbd3c310d3dae351f40e88a5695dbccee9
gumasaje <geniarti@naver.com>
2025-01-15 08:40:04 +0900

    [Bronze V] Title: ??!, Time: 112 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,11 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
-        String password = in.next();
-        System.out.print(password + "??!");
-        in.close();
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		System.out.print(str+"??!");
+		
+	}
 }
