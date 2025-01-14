fa51b24bab178e13765ea3a6f847cf0c2386ea11
gumasaje <geniarti@naver.com>
2025-01-15 08:31:34 +0900

    [Bronze V] Title: A/B, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) {
-        
-        Scanner in = new Scanner(System.in);
-        double A = in.nextDouble();
-        double B = in.nextDouble();
-        
-        System.out.print(A/B);
-        
-        in.close();
-    }
+public class Main{
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		double A = Double.parseDouble(st.nextToken());
+		double B = Double.parseDouble(st.nextToken());
+		
+		System.out.print(A/B);
+		
+	}
 }
