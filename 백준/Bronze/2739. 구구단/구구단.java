f080517e95f549052a9980a465d4932af12a2426
gumasaje <geniarti@naver.com>
2025-01-15 09:31:29 +0900

    [Bronze V] Title: 구구단, Time: 128 ms, Memory: 16200 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,12 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        sc.close();
-        
-        for(int i=1; i<=9; i++)
-            System.out.println(N+" * "+i+" = "+N*i);
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=9; i++)
+			System.out.println(N+" * "+i+" = "+N*i);
+		
+	}
 }
