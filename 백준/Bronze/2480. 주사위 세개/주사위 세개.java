ef18cc51dfe8f3c1edeb8eb186f2e322ee2460e8
gumasaje <geniarti@naver.com>
2025-01-15 09:29:07 +0900

    [Bronze IV] Title: 주사위 세개, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,26 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int prize = 0;
-        sc.close();
-        
-        if(a==b && b==c)
-            prize = 10000+(a*1000);
-        else if(a==b || a==c)
-            prize = 1000+(a*100);
-        else if(b==c)
-            prize = 1000+(b*100);
-        else if(a>b && a>c)
-            prize = a*100;
-        else if(b>a && b>c)
-            prize = b*100;
-        else if(c>a && c>b)
-            prize = c*100;
-        
-        System.out.print(prize);
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int prize = 0;
+		
+		if(A==B && A==C)
+			prize = 10000 + (A*1000);
+		else if(A==B || A==C)
+			prize = 1000 + (A*100);
+		else if(B==C)
+			prize = 1000 + (B*100);
+		else if(A>B && A>C)
+			prize = A*100;
+		else if(B>A && B>C)
+			prize = B*100;
+		else
+			prize = C*100;
+		
+		System.out.print(prize);
+		
+	}
 }
