cfa5f200a4eb6df066c5a34b9ce908646fd77db4
gumasaje <geniarti@naver.com>
2025-01-15 10:32:55 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,23 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Main{
-    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int arr[] = new int[N];
-        
-        for(int i=0; i<arr.length; i++)
-            arr[i] = sc.nextInt();
-        
-        int v = sc.nextInt();
-        sc.close();
-        
-        int count = 0;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == v)
-                count += 1;
-        }
-        
-        System.out.print(count);
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int arr[] = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int v = Integer.parseInt(br.readLine());
+		int count = 0;
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			if(arr[i] == v) {
+				count++;
+			}
+			
+		}
+		
+		System.out.print(count);
+		
+	}
 }
