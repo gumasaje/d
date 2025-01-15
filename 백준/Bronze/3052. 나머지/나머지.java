3a0ad795a9469650659a73c091d5b0b0c8612382
gumasaje <geniarti@naver.com>
2025-01-15 11:35:10 +0900

    [Bronze II] Title: 나머지, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,25 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
-        
-        int count = 0;
-        int mod[] = new int[10];
-        
-        for(int i=0; i<10; i++)
-            mod[i] = sc.nextInt()%42;
-        sc.close();
-        
-        for(int i=0; i<10; i++){
-            int temp = 0;
-            for(int j=i+1; j<10; j++){
-                if(mod[i]==mod[j])
-                    temp++;
-            }
-            if(temp==0)
-                count++;
-        }
-        System.out.print(count);
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int arr[] = new int[10];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine())%42;
+		}
+		
+		int count = 0;
+		boolean bl;
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			bl = false;
+			
+			for(int j=i+1; j<arr.length; j++) {
+				
+				if(arr[i] == arr[j]) {
+					bl = true;
+					break;
+				}
+			}
+			
+			if(bl == false) {
+				count ++;
+			}
+		}
+		System.out.print(count);
+	}
 }
