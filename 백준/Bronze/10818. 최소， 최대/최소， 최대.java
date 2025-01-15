348f7317477c73003ce3bba8298594cdfe4bb277
gumasaje <geniarti@naver.com>
2025-01-15 10:53:38 +0900

    [Bronze III] Title: 최소, 최대, Time: 1196 ms, Memory: 105536 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,28 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
 public class Main{
-    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int Array[] = new int[N];
-        
-        for(int i=0; i<Array.length; i++)
-            Array[i] = sc.nextInt();
-        sc.close();
-        
-        int max = Array[0];
-        int min = Array[0];
-        
-        for(int j=0; j<Array.length; j++){
-            if(Array[j]<min)
-                min = Array[j];
-        }
-        
-        for(int k=0; k<Array.length; k++){
-            if(Array[k]>max)
-                max = Array[k];
-        }
-        
-        System.out.print(min+" "+max);
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int arr[] = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		System.out.print(arr[0]+" "+arr[N-1]);
+	}
 }
