a6f430462ccf18b8b115384869fa860261da6a75
gumasaje <geniarti@naver.com>
2025-01-15 11:21:40 +0900

    [Bronze II] Title: 공 바꾸기, Time: 120 ms, Memory: 15892 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,25 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int arr[] = new int[N];
-        
-        for(int i=0; i<N; i++)
-            arr[i] = i+1;
-        
-        for(int i=0; i<M; i++){
-            int I = sc.nextInt();
-            int J = sc.nextInt();
-            int K = arr[I-1];
-            arr[I-1] = arr[J-1];
-            arr[J-1] = K; 
-        }
-        sc.close();
-        
-        for(int i=0; i<N; i++)
-            System.out.print(arr[i]+" ");
-                           
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int arr[] = new int[N];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = i+1;
+		}
+		
+		for(int i=0; i<M; i++) {
+			
+			st = new StringTokenizer(br.readLine());
+			int I = Integer.parseInt(st.nextToken());
+			int J = Integer.parseInt(st.nextToken());
+			int K = arr[I-1];
+			
+			arr[I-1] = arr[J-1];
+			arr[J-1] = K;
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
 }
