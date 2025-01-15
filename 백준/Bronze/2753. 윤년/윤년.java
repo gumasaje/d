91280580120e8212268a08bd1e9f58791af457f1
gumasaje <geniarti@naver.com>
2025-01-15 09:10:07 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,20 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int year = sc.nextInt();
-        sc.close();
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int year = Integer.parseInt(br.readLine());
+		
+		if(year%4 == 0) {
+			if(year%100 != 0 || year%400 == 0)
+				System.out.print('1');
+			else
+				System.out.print('0');
+		}
         
-        if(year%4==0){
-            if(year%100!=0)
-                System.out.print("1");
-            else if(year%400==0)
-                System.out.print("1");
-            else
-                System.out.print("0");
-        }
-        else
-            System.out.print("0");
-    }
+		else
+			System.out.print('0');
+		
+	}
 }
