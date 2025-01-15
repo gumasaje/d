c79b2ebe689e3f016a6145199c0571fd933bf849
gumasaje <geniarti@naver.com>
2025-01-15 09:41:59 +0900

    [Bronze IV] Title: 영수증, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,23 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Main{
-    public static void main(String[] args){
-        int sum = 0;
-        
-        Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt();
-        int N = sc.nextInt();
-        
-        for(int i=1; i<=N; i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            sum += a*b;
-        }
-        sc.close();
-        
-        if(sum==X)
-            System.out.print("Yes");
-        else
-            System.out.print("No");
-        
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int X = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int sum = 0;
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			sum += Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
+		}
+		
+		if(X == sum)
+			System.out.print("Yes");
+		else
+			System.out.print("No");
+		
+	}
 }
