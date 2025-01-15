6e672f3f8cbb942ee03b0fb6fd1eb3ef591a8e29
gumasaje <geniarti@naver.com>
2025-01-15 09:37:15 +0900

    [Bronze V] Title: 합, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,15 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int sum = 0;
-        sc.close();
-        
-        for(int i=1; i<=n; i++)
-            sum += i;
-        
-        System.out.print(sum);
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int sum = 0;
+		
+		for(int i=1; i<=n; i++)
+			sum += i;
+		
+		System.out.print(sum);
+		
+	}
 }
