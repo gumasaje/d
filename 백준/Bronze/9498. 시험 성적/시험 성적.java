2516dd6e28bdf9ad94698a11403b6eb6053c27dc
gumasaje <geniarti@naver.com>
2025-01-15 09:06:19 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,19 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int score = sc.nextInt();
-        sc.close();
-        if(score>=90)
-            System.out.print("A");
-        else if(score>=80)
-            System.out.print("B");
-        else if(score>=70)
-            System.out.print("C");
-        else if(score>=60)
-            System.out.print("D");
-        else
-            System.out.print("F");
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int score = Integer.parseInt(br.readLine());
+		
+		if(90 <= score && score <= 100)
+			System.out.print('A');
+		else if(80 <= score && score <= 89)
+			System.out.print('B');
+		else if(70 <= score && score <= 79)
+			System.out.print('C');
+		else if(60 <= score && score <= 69)
+			System.out.print('D');
+		else
+			System.out.print('F');
+		
+	}
 }
