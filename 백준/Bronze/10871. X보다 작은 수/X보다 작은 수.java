e6e3767a7d719ffddd71196a82c98d78d019c6cb
gumasaje <geniarti@naver.com>
2025-01-15 10:41:54 +0900

    [Bronze V] Title: X보다 작은 수, Time: 276 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,20 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		
+		int arr[] = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			arr[i] = Integer.parseInt(st.nextToken());
+			
+			if(arr[i] < X) {
+				System.out.print(arr[i]+" ");
+			}
+			
+		}
         
-        int N = sc.nextInt();
-        int X = sc.nextInt();
-        
-        int A[] = new int[N];
-        for(int i=0; i<N; i++)
-            A[i] = sc.nextInt();
-        
-        for(int i=0; i<N; i++){
-            if(A[i] < X)
-                System.out.print(A[i]+" ");
-        }
-        
-        sc.close();
-    }
+	}
 }
