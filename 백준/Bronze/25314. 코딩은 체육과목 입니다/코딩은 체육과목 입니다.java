5f8fcaa7a8e16b00508d22f72e1a9a6e85f3ea61
gumasaje <geniarti@naver.com>
2025-01-15 09:51:04 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 112 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,13 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        sc.close();
-        
-        for(int i=1; i<=N; i+=4){
-            System.out.print("long ");
-        }
-        System.out.print("int");
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N/4; i++)
+			System.out.print("long ");
+		
+		System.out.print("int");
+			
+	}
 }
