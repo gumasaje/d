edf91603b607149d12a96ba14a2069010a828add
gumasaje <geniarti@naver.com>
2025-01-17 17:08:21 +0900

    [Bronze V] Title: 문자열, Time: 100 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        
-        for(int i=0; i<T; i++){
-            String str = sc.next();
-            System.out.print(str.charAt(0));
-            System.out.println(str.charAt(str.length()-1));
-        }
-        sc.close();
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<T; i++) {
+			String str = br.readLine();
+			System.out.print(str.charAt(0));
+			System.out.println(str.charAt(str.length()-1));
+		}
+	}
 }
