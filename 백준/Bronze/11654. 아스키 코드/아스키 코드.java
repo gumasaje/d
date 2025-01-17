e77a923880fdb947448087a41d4ad9518fb395b0
gumasaje <geniarti@naver.com>
2025-01-17 17:12:55 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        sc.close();
-        
-        int n = str.charAt(0);
-        
-        System.out.print(n);
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		int n = str.charAt(0);
+		
+		System.out.print(n);
+	}
 }
