d7f5602bfc03a5f4d2428f02fc67c728da7ae9c6
gumasaje <geniarti@naver.com>
2025-01-17 17:27:25 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 116 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,21 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args){
-        int arr[] = new int[26];
-        for(int i=0; i<arr.length; i++)
-            arr[i] = -1;
-        
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        sc.close();
-        
-        for(int i=0; i<str.length(); i++){
-            char ch = str.charAt(i);
-            if(arr[ch-'a'] == -1)
-                arr[ch-'a'] = i;
-        }
-        
-        for(int i=0; i<arr.length; i++)
-            System.out.print(arr[i]+" ");
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int arr[] = new int[26];
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = -1;
+		}
+		
+		String str = br.readLine();
+		
+		for(int i=0; i<str.length(); i++) {
+			char ch = str.charAt(i);
+			if(arr[ch-'a'] == -1) {
+				arr[ch-'a'] = i;
+			}
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
 }
