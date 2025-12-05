2794fef7ad1ca748cc1fef9a82807b71a770a95b
gumasaje <191728697+gumasaje@users.noreply.github.com>
2025-12-05 18:52:51 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.09 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,9 +1,7 @@
 class Solution {
     public String solution(String my_string, int k) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < k; i++) {
-            for(int j = 0; j < my_string.length(); j++) sb.append(my_string.charAt(j));
-        }
+        for(int i = 0; i < k; i++) sb.append(my_string);
         return sb.toString();
     }
 }
