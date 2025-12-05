af44064b86bd5b79e34fbd21b758f709ad1c9ddf
gumasaje <191728697+gumasaje@users.noreply.github.com>
2025-12-05 19:03:25 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.78 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,16 +1,7 @@
 class Solution {
     public int solution(int a, int b) {
-        StringBuilder sb =  new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        
-        sb.append(a);
-        sb.append(b);
-        
-        sb2.append(b);
-        sb2.append(a);
-        
-        if(Integer.parseInt(sb.toString()) > Integer.parseInt(sb2.toString())) return Integer.parseInt(sb.toString());
-        else if(Integer.parseInt(sb.toString()) < Integer.parseInt(sb2.toString())) return Integer.parseInt(sb2.toString());
-        else return Integer.parseInt(sb.toString());
+        int ab = Integer.parseInt(Integer.toString(a) + Integer.toString(b));
+        int ba = Integer.parseInt(Integer.toString(b) + Integer.toString(a));
+        return ab >= ba ? ab : ba;
     }
 }
