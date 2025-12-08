0387d500c38f68e4b508684511eab507d8f8b1bb
gumasaje <191728697+gumasaje@users.noreply.github.com>
2025-12-08 20:43:58 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] num_list) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 == 0){
+                sb.append(num_list[i]);
+            }
+            else {
+                sb2.append(num_list[i]);
+            }
+        }
+        
+        return Integer.parseInt(sb.toString()) + Integer.parseInt(sb2.toString());
+    }
+}
