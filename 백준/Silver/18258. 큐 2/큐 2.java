4d1e44e2131d4b6188b99010f65bc52288404e7e
gumasaje <191728697+gumasaje@users.noreply.github.com>
2025-12-08 23:01:21 +0900

    [Silver IV] Title: 큐 2, Time: 1256 ms, Memory: 334256 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Integer> q = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            switch(command) {
+                case "push":
+                    q.offer(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    if(!q.isEmpty()) {
+                        sb.append(q.poll()).append("\n");
+                    }
+                    else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(q.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(q.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    if(!q.isEmpty()) {
+                        sb.append(q.peek()).append("\n");
+                    }
+                    else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case "back":
+                    if(!q.isEmpty()) {
+                        sb.append(q.getLast()).append("\n");
+                    }
+                    else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
