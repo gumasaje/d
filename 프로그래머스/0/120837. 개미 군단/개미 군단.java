4b250c47c3a7c00f2c453c7ed63e9f61690d6dd5
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-02-25 00:22:21 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
-        return hp / 5 + hp % 5 / 3 + hp % 5 % 3 / 1;
+        return hp / 5 + hp % 5 / 3 + hp % 5 % 3;
     }
 }
