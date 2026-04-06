c512b692626132662dc36af71839b4ea1a6dfd8a
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-04-06 14:53:50 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.11 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -3,26 +3,13 @@ class Solution {
         int answer = 0;
 
         for (int i = left; i <= right; i++) {
-            int countDivisors = countDivisors(i);
-            answer += countDivisors % 2 == 0 ? i : -i;
-        }
-
-        return answer;
-    }
-
-    private static int countDivisors(int num) {
-        int count = 0;
-
-        for (int i = 1; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                if (i * i == num) {
-                    count++;
-                } else {
-                    count += 2;
-                }
+            if (i % Math.sqrt(i) == 0) {
+                answer -= i;
+            } else {
+                answer += i;
             }
         }
         
-        return count;
+        return answer;
     }
 }
