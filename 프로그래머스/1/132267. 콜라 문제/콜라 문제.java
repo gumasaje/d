0bdb3325d19b776dc360ddc1e2e6494bf1b07c57
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-04-14 19:33:27 +0900

    [level 1] Title: 콜라 문제, Time: 0.11 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        int bottles = n;
 
-        while (bottles >= a) {
-            answer += b * (bottles / a);
-            bottles = bottles - a * (bottles / a) + b * (bottles / a);
+        while (n >= a) {
+            int newBottles = b * (n / a);
+            answer += newBottles;
+            n = n % a + newBottles;
         }
 
         return answer;
