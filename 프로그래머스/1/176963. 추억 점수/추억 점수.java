1c92cdea38ef8937cd0d89202aa1d754c6907b09
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-04-20 12:47:42 +0900

    [level 1] Title: 추억 점수, Time: 1.52 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
+
         HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < name.length; i++) {
             map.put(name[i], yearning[i]);
@@ -10,10 +11,8 @@ class Solution {
 
         for (int i = 0; i < photo.length; i++) {
             int score = 0;
-            for (int j = 0; j < photo[i].length; j++) {
-                if (map.containsKey(photo[i][j])) {
-                    score += map.get(photo[i][j]);
-                }
+            for (String key : photo[i]) {
+                score += map.getOrDefault(key, 0);
             }
             answer[i] = score;
         }
