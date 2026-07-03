d87b8689b8a393f865053edb47832c285192dc68
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-07-03 15:41:56 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,31 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = new int[2];
-        int count = 0;
         int zeroCount = 0;
+        int count = 0;
 
+        Set<Integer> set = new HashSet<>();
         for (int i : lottos) {
-            if (i == 0) {
-                zeroCount++;
-                continue;
-            }
-            for (int j : win_nums) {
-                if (i == j)
-                    count++;
-            }
+            if (i == 0) zeroCount++;
+            else set.add(i);
         }
-        
+
+        for (int i : win_nums) if (set.contains(i)) count++;
+
         int maxMatch = count + zeroCount;
         int minMatch = count;
 
-        answer[0] = rank(maxMatch);
-        answer[1] = rank(minMatch);
-
-
-        return answer;
+        return new int[]{getRank(maxMatch), getRank(minMatch)};
     }
 
-    private int rank(int count) {
+    private int getRank(int count) {
         return count == 0 ? 6 : 7 - count;
     }
 }
