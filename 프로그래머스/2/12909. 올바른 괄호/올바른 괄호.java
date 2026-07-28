1c11a42027d20f893b911e0af62be455c9a4ecc9
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-07-28 12:31:47 +0900

    [level 2] Title: 올바른 괄호, Time: 3.08 ms, Memory: 57.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+class Solution {
+    boolean solution(String s) {
+        int balance = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') balance++;
+            else if (s.charAt(i) == ')') {
+                balance--;
+
+                if (balance < 0) return false;
+            }
+        }
+
+        return balance == 0;
+    }
+}
